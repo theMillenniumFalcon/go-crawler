f985b2289b509fd468471d2630146f2ed9f3af85
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-07-10 03:51:59 +0530

    messages bow echoing

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
 func handleConnection(conn net.Conn) error {
 	log.Println("I got a connection")
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			log.Println(buf)
+			break
+		}
+		msg := buf[0 : n-2]
+		log.Println("Receivedd message: ", []byte(msg))
+
+		resp := fmt.Sprintf("You said, \"%s\"\r\n", msg)
+		n, err = conn.Write([]byte(resp))
+		if n == 0 {
+			log.Println("Zero bytes, closing connection")
+			break
+		}
+	}
 	return nil
 }
 
